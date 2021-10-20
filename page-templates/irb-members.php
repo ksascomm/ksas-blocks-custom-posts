17854255ed64bc9a8dcfb12d0a50cedc54880eea
ksascomm <ksasweb@jhu.edu>
2021-10-20 09:32:47 -0400

    Move query to template-part

--- page-templates/irb-members.php
+++ page-templates/irb-members.php
@@ -61,41 +61,7 @@ $related_members_query = new WP_Query(
 			while ( $members_query->have_posts() ) :
 				$members_query->the_post();
 				?>
-			<div class="tab w-full overflow-hidden">
-				<input class="absolute opacity-0" id="<?php the_ID(); ?>" type="radio" name="tabs2">
-				<label class="block p-5 leading-normal cursor-pointer font-semibold" for="<?php the_ID(); ?>"><?php the_title(); ?></label>
-				<div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-blue-500 leading-normal">
-					<?php the_content(); ?>
-					<?php if ( get_field( 'document_upload' ) ) : ?>
-						<p><a href="<?php the_field( 'document_upload' ); ?>">View the Document</a></p>
-					<?php endif; ?>
-					<?php if ( get_field( 'resource_link' ) ) : ?>
-						<p><a href="http://<?php the_field( 'resource_link' ); ?>">Read Additional <?php the_title(); ?> Information</a></p>
-					<?php endif; ?>
-					<?php if ( get_edit_post_link() ) : ?>
-						<footer class="entry-footer">
-							<?php
-							edit_post_link(
-								sprintf(
-									wp_kses(
-										/* translators: %s: Name of current post. Only visible to screen readers */
-										__( 'Edit Document <span class="sr-only">%s</span>', 'ksas-blocks' ),
-										array(
-											'span' => array(
-												'class' => array(),
-											),
-										)
-									),
-									wp_kses_post( get_the_title() )
-								),
-								'<span class="edit-link">',
-								'</span>'
-							);
-							?>
-						</footer><!-- .entry-footer -->
-					<?php endif; ?>
-				</div>
-			</div>
+				<?php get_template_part( 'template-parts/content', 'documents-accordion' ); ?>
 			<?php endwhile; ?>
 		<?php endif; ?>
 		<?php
@@ -106,41 +72,7 @@ $related_members_query = new WP_Query(
 			while ( $related_members_query->have_posts() ) :
 				$related_members_query->the_post();
 				?>
-			<div class="tab w-full overflow-hidden">
-				<input class="absolute opacity-0" id="<?php the_ID(); ?>" type="radio" name="tabs2">
-				<label class="block p-5 leading-normal cursor-pointer font-semibold" for="<?php the_ID(); ?>"><?php the_title(); ?></label>
-				<div class="tab-content overflow-hidden border-l-2 bg-gray-100 border-blue-500 leading-normal">
-					<?php the_content(); ?>
-					<?php if ( get_field( 'document_upload' ) ) : ?>
-						<p><a href="<?php the_field( 'document_upload' ); ?>">View the Document</a></p>
-					<?php endif; ?>
-					<?php if ( get_field( 'resource_link' ) ) : ?>
-						<p><a href="http://<?php the_field( 'resource_link' ); ?>">Read Additional <?php the_title(); ?> Information</a></p>
-					<?php endif; ?>
-					<?php if ( get_edit_post_link() ) : ?>
-						<footer class="entry-footer">
-							<?php
-							edit_post_link(
-								sprintf(
-									wp_kses(
-										/* translators: %s: Name of current post. Only visible to screen readers */
-										__( 'Edit Document <span class="sr-only">%s</span>', 'ksas-blocks' ),
-										array(
-											'span' => array(
-												'class' => array(),
-											),
-										)
-									),
-									wp_kses_post( get_the_title() )
-								),
-								'<span class="edit-link">',
-								'</span>'
-							);
-							?>
-						</footer><!-- .entry-footer -->
-					<?php endif; ?>
-				</div>
-			</div>
+				<?php get_template_part( 'template-parts/content', 'documents-accordion' ); ?>
 			<?php endwhile; ?>
 		<?php endif; ?>
 		<?php wp_reset_postdata();  // Restore global post data stomped by the_post(). ?>
