a245e9bb162c63e4aa70c548d9c290c12b621da3
ksascomm <ksasweb@jhu.edu>
2021-11-10 11:13:34 -0500

    Add Spotlight archives template; new card styles

---
+++ taxonomy-profiletype.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying Spotlight archives
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package KSAS_Blocks
+ */
+
+get_header();
+?>
+
+	<main id="site-content" class="site-main prose sm:prose lg:prose-lg mx-auto">
+		<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+		<?php
+		if ( function_exists( 'bcn_display' ) ) {
+			bcn_display();
+		}
+		?>
+		</div>
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header prose px-2 py-6">
+				<?php
+				the_archive_title( '<h1 class="entry-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+			<div class="flex flex-wrap">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', 'profile-card' );
+
+				endwhile;
+				?>
+			</div>
+			<?php
+			if ( function_exists( 'ksas_blocks_pagination' ) ) :
+
+				ksas_blocks_pagination();
+
+			else :
+
+				the_posts_navigation();
+
+			endif;
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
--- template-parts/content-profile-card.php
+++ template-parts/content-profile-card.php
@@ -11,12 +11,12 @@
 
 <div class="profile-card p-2 w-full md:w-1/3">
 	<div class="h-full mb-4 px-6 py-4">
-		<div class="w-64 h-64 bg-cover bg-center rounded" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>)">
+		<div class="h-80 bg-cover rounded" style="background-image: url(<?php the_post_thumbnail_url( 'large' ); ?>)">
 		</div>
 		<h3>
 			<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
 		</h3>
-		<h4><?php the_field('fields_of_study'); ?></h4>
+		<h4><?php the_field( 'fields_of_study' ); ?></h4>
 		<div class="flex items-center flex-wrap">
 			<?php if ( get_post_meta( $post->ID, 'ecpt_pull_quote', true ) ) : ?>
 				<?php echo get_post_meta( $post->ID, 'ecpt_pull_quote', true ); ?>
