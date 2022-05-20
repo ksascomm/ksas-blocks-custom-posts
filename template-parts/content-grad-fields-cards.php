7d5ed644e7a08c1a3f77b8626b72ac9ccb0b67d9
ksascomm <ksasweb@jhu.edu>
2022-05-20 09:16:24 -0400

    Add ID to cards

--- template-parts/content-grad-fields-cards.php
+++ template-parts/content-grad-fields-cards.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<div class="graduate-field-card p-2 w-full md:w-1/3 item">
+<div class="graduate-field-card p-2 w-full md:w-1/3 item" id="<?php the_title(); ?>">
 	<div class="h-full rounded-lg overflow-hidden field  mb-4 px-6 py-4 overflow-hidden bg-white graduate-field-card-outline">
 		<div class="p-8">
 			<h3 class="font-heavy font-bold">
