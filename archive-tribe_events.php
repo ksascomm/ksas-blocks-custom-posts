6e30f77320ef8f4a1c1c0a15e82d899cb36eac1b
ksascomm <ksasweb@jhu.edu>
2022-07-11 10:28:15 -0400

    Check in new archive template

---
+++ archive-tribe_events.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying Tribe Events archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package KSAS_Blocks
+ */
+
+get_header();
+?>
+
+	<main id="site-content" class="site-main prose sm:prose lg:prose-lg mx-auto">
+		<?php
+		if ( function_exists( 'bcn_display' ) ) :
+			?>
+		<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php bcn_display(); ?>
+		</div>
+		<?php endif; ?>
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header prose px-2 py-6">
+				<?php
+				the_archive_title( '<h1 class="entry-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
